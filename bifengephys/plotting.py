d02da7dece3eea0193b6737aca926483976072af
Friedrich Fu <zhehfu@student.ethz.ch>
2021-05-03 20:34:04 +0200

    create wrapper in plotting.py

--- bifengephys/plotting.py
+++ bifengephys/plotting.py
@@ -92,3 +92,48 @@ def plot_mean_ci(data, duration=300):
     plt.fill_between(t[:duration * srate], ci95[0][:duration * srate], ci95[1][:duration * srate], alpha=0.3)
     plt.show()
     
+
+def plot_phase_coh_pairs(data, animal, session, savedir, band='theta', srate=500, tstart=30, twin=600, axs=None, showfig=True):
+    phase_mpfc = ephys.column_by_pad(ephys.get_phase(data, 'mpfc', 'theta'))
+    phase_vhipp = ephys.column_by_pad(ephys.get_phase(data, 'vhipp', 'theta'))
+    mpfc_pads = np.array(phase_mpfc.columns)
+    vhipp_pads = np.array(phase_vhipp.columns)
+
+    FWHMs = []
+    for i in range(len(mpfc_pads)):
+        FWHM = ephys.plot_phase_coh(data, 
+                                    fname=savedir+animal[session]+'_mPFC_pad'+str(mpfc_pads[i])+'_phasecoh.jpg', 
+                                    band='theta', mpfc_index=i, srate=srate, 
+                                    tstart=tstart, twin=twin)
+        FWHMs.append(FWHM)
+
+    FWHMs_np = np.array(FWHMs)
+    mpfc_ch_labels = ['pad'+str(mpfc_pads[i]) for i in range(len(mpfc_pads))]
+    vhipp_ch_labels = ['pad'+str(vhipp_pads[i]) for i in range(len(vhipp_pads))]
+
+    fig, ax = plt.subplots(figsize=(10,10))
+    im = ax.imshow(FWHMs_np, vmin=np.min(FWHMs_np), vmax=np.max(FWHMs_np))
+
+    ax.set_xticks(np.arange(len(vhipp_ch_labels)))
+    ax.set_yticks(np.arange(len(mpfc_ch_labels)))
+
+    ax.set_xticklabels(vhipp_ch_labels)
+    ax.set_yticklabels(mpfc_ch_labels)
+
+    for tick in ax.xaxis.get_major_ticks():
+        tick.label.set_fontsize(10)
+        tick.label.set_rotation('vertical')
+
+    for tick in ax.yaxis.get_major_ticks():
+        tick.label.set_fontsize(10)
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    fig.tight_layout()
+    fig.colorbar(im, shrink=0.5)
+    plt.title('Heatmap of FWHM values of all mPFC and vHPC channel pairs', fontsize=16)
+    plt.xlabel('vHPC')
+    plt.ylabel('mPFC')
+    plt.savefig(savedir+animal[session]+'_phasecoh_fwhms.jpg')
+    plt.show()
